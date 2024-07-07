754ef0faa10b5019eb93566a4be58440faee7969
DiweiGithub <107074842+DiweiGithub@users.noreply.github.com>
2024-07-07 21:19:03 +0000

    update

--- pages/reset.py
+++ pages/reset.py
@@ -22,16 +22,22 @@ try:
         with open('data/Admin.yaml', 'w') as file:
             yaml.dump(config, file, default_flow_style=False)
         st.success('User registered successfully')
-        uploaded_photo = st.file_uploader("Upload a photo")
-        camera_photo = st.camera_input("Take a photo")
-        progress_bar = st.progress(0)
-        for perc_completed in range(100):
-            time.sleep(0.05)
-            progress_bar.progress(perc_completed+1)
-        if uploaded_photo is None:
-            st.image(camera_photo)
-        else:
-            st.image(uploaded_photo)
+
+        if "photo" not in st.session_state:
+            st.session_state["photo"]="not done"
+        def change_photo_state():
+            st.session_state["photo"] =='done'
+        uploaded_photo = st.file_uploader("Upload a photo",on_change=change_photo_state)
+        camera_photo = st.camera_input("Take a photo",on_change=change_photo_state)
+        if st.session_state["photo"] =="done":
+            progress_bar = st.progress(0)
+            for perc_completed in range(100):
+                time.sleep(0.05)
+                progress_bar.progress(perc_completed+1)
+            if uploaded_photo is None:
+                st.image(camera_photo)
+            else:
+                st.image(uploaded_photo)
         #st.switch_page("streamlit_app.py")   
 except Exception as e:
     st.error(e)
